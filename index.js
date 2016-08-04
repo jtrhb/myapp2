b360fa8e6b0ce6bc7b1340b7486177c98f02ab1a
jtrhb <jtrhb@users.noreply.github.com>
2016-08-04 22:53:57 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -10,85 +10,97 @@ var sql_host = 'localhost';
 var sql_user = 'root';
 var sql_psd = 'root';
 var sql_db = 'test';
-var sql_conn = {connectionLimit:sql_cLimit, host:sql_host, user:sql_user, password:sql_psd, database:sql_db};
+var sql_multiple = true;
+var sql_conn = {connectionLimit:sql_cLimit, host:sql_host, user:sql_user, password:sql_psd, database:sql_db, multipleStatements:sql_multiple};
 var pool = mysql.createPool(sql_conn);
-var jsonResponse, minTickCount, chartData, allConnections, lastTickData;
+var jsonResponse, minTickCount, chartData, allConnections = [], lastTickData;
+var bid = 0, ask = 0, spread = 0, shouldEmitRealtimeUpdate = 1;
+var longCount = 10, shortCount = 10;
 
 pool.getConnection(function(err,connection) {
   if(err) {
     console.error('error connecction;'+err.stack);
     return;
   } else {
-    var sql_stmt = 'SELECT * FROM `forexm1`';
+    var sql_stmt = 'SELECT * FROM `forexm1`; SELECT * FROM `forexsec`';
     connection.query(sql_stmt, function(err, rows, fields) {
       if(err) {
         throw err;
       } else {
-        minTickCount = rows.length;
-        chartData = rows;
-        jsonResponse = JSON.stringify(rows);
+        minTickCount = rows[0].length;
+        chartData = rows[0];
+        lastTickData = rows[1];
+        chartData.push(lastTickData[0]);
+        //jsonResponse = JSON.stringify(rows);
         connection.release();
-        getSecUpdate();
-        checkMinUpdate();
+        tickUpdate();
+        realtimeUpdate();
       }
     });
   }
 });
 
-function checkMinUpdate() {
-  var minUpdateStmt = 'SELECT ID FROM `forexm1` DESC LIMIT 0,1';
+function realtimeUpdate() {
   pool.getConnection(function(err, connection) {
-    connection.query(minUpdateStmt, function(err, rows, fields) {
-      if(err) {
-        throw err;
-      } else {
-        connection.release();
-        if(rows[0]['ID'] > minTickCount) {
-          getMinUpdate;
+    var realtimesql_stmt = 'SELECT * FROM `forexrealtime`';
+    connection.query(realtimesql_stmt, function(err, rows, fields) {
+        if(err) {
+          throw err;
         } else {
-          setTimeout(checkMinUpdate, 1000);
+          connection.release();
+          if(bid != Number(rows[0]['Bid']) || ask != Number(rows[0]['Ask'])) {
+            bid = Number(rows[0]['Bid']);
+            ask = Number(rows[0]['Ask']);
+            if(allConnections.length) {
+                io.emit('bid ask update', JSON.stringify(rows[0]));
+              }
+          }
+          setTimeout(realtimeUpdate, 100);
         }
-      }
-    });
-  });  
+      });
+  });
 }
 
-function getMinUpdate() {
-  var minUpdateStmt = 'SELECT * FROM `forexm1` DESC LIMIT 0,1';
+function tickUpdate() {
   pool.getConnection(function(err, connection) {
-    connection.query(minUpdateStmt, function(err, rows, fields) {
+    var secUpdateStmt = 'SELECT * FROM `forexsec`';
+    var checkMinUpdateStmt = 'SELECT `ID` FROM `forexm1` ORDER BY `ID` DESC LIMIT 0,1';
+    var minUpdateStmt = 'SELECT * FROM `forexm1` ORDER BY `ID` DESC LIMIT 0,1';
+    var jointStmt1 = 'SELECT * FROM `forexsec`; SELECT `ID` FROM `forexm1` ORDER BY `ID` DESC LIMIT 0,1';
+    var jointStmt2 = 'SELECT * FROM `forexsec`; SELECT * FROM `forexm1` ORDER BY `ID` DESC LIMIT 0,1';
+    connection.query(jointStmt1, function(err, rows, fields) {
       if(err) {
         throw err;
       } else {
-        chartData.push(rows);
-        connection.release();
-        if(allConnections.length) {
-          io.emit('min chart update', JSON.stringify(rows));
-          setTimeout(checkMinUpdate,60000);
-        } 
-      }
-    });
-  }); 
-}
-
-function getSecUpdate() {
-  var secUpdateStmt = 'SELECT * FROM `forexsec`';
-  pool.getConnection(function(err, connection) {
-    connection.query(secUpdateStmt, function(err, rows, fields) {
-      if(err) {
-        throw err;
-      } else {
-        lastTickData = rows;
-        connection.release();
-        if(allConnections.length) {
-          io.emit('sec chart update', JSON.stringify(rows));
-          setTimeout(getSecUpdate(), 1000);
+        if(rows[1][0]['ID'] > minTickCount) {
+          connection.query(jointStmt2, function(err, rows, fields) {
+            if(err) {
+              throw err;
+            } else {
+              connection.release();
+              lastTickData = rows[0];
+              chartData[chartData.length - 1] = rows[1][0];
+              chartData.push(lastTickData[0]);
+              minTickCount += 1;
+              if(allConnections.length) {
+                io.emit('min chart update', {min:JSON.stringify(rows[1]), sec:JSON.stringify(lastTickData)});
+              }
+              setTimeout(tickUpdate,1000);
+            }
+          });
+        } else {
+          lastTickData = rows[0];
+          chartData[chartData.length - 1] = lastTickData[0];
+          if(allConnections.length) {
+            io.emit('sec chart update', JSON.stringify(lastTickData));
+          }
+          connection.release();
+          setTimeout(tickUpdate,1000);
         }
       }
     });
   });
 }
-
 
 app.use(express.static(path.join(__dirname, 'public'))); //设置环境变量
 app.get('/', function(req, res){
@@ -98,19 +110,26 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('a user connected');
   allConnections.push(socket);
+  socket.emit('update bar percentage', {"long":longCount,"short":shortCount});
   socket.emit('init chart update', JSON.stringify(chartData));
+  socket.emit('bid ask update', JSON.stringify({Bid:String(bid),Ask:String(ask)}));
   socket.on('chat message', function(msg){
       io.emit('chat message', msg);
   });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+  socket.on('vote for', function(msg) {
+    if(msg == "long") {
+      longCount += 1;
+      io.emit("update long percentage",longCount);
+    } else {
+      shortCount += 1;
+      io.emit("update short percentage", shortCount);
+    }
+  })
 });
 
-setInterval(function() {
-  io.emit('chat message', '');
-},1000);
-
-http.listen(80, function(){
-  console.log('listening on *:80');
+http.listen(911, function(){
+  console.log('listening on *:911');
 });
